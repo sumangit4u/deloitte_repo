5dddfd2bf198649e9f0840e88ace43f0151563d5
Suman Das <40280421+sumangit4u@users.noreply.github.com>
2021-08-29 10:25:35 +0530

    Update launch_docker.sh

--- launch_docker.sh
+++ launch_docker.sh
@@ -2,4 +2,4 @@
 sudo docker pull sumandocker4u/deloitte:latest
 sudo docker container run --publish 5049:5000 --detach sumandocker4u/deloitte:latest
 echo "Docker running on localhost port 5049"
-sudo curl "http:localhost:5049/"
+sudo curl "http://localhost:5049"
